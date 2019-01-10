a4abca1adf2462878e254e63f59e1aeea7fbc7e7
deadzq <deadzq@qq.com>
2019-01-10 10:37:57 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+##### 2019-1-10
+- 1.controller: ShowPageController页面展示.
+- 2.展示暂时有resources/templdates/[index,about,services,contact]
+- 3.新导入依赖有pagehelper,MySQL连接,thymeleaf,lombok,starter-mail.
+- four.[前端页面选型](https://startbootstrap.com/template-overviews/modern-business/)
---
+++ src/main/java/com/tansuo365/test1/controller/ShowPageController.java
@@ -0,0 +1,48 @@
+package com.tansuo365.test1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//TODO 将title改为后台页面设定
+@Controller
+public class ShowPageController {
+
+    /*展示首页*/
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("title","碳索365 测试网页!");
+        return "index";
+    }
+
+    /*about介绍页面*/
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title","关于碳索365");
+        return "about";
+    }
+
+    /*services*/
+    @RequestMapping("/services")
+    public String services(Model model){
+        model.addAttribute("title","services");
+        return "services";
+    }
+
+    /*contact*/
+    @RequestMapping("/contact")
+    public String contact(Model model){
+        model.addAttribute("title","contact");
+        return "contact";
+    }
+
+    //TODO 其它待定页面
+    @RequestMapping("/others")
+    public String others(Model model){
+        model.addAttribute("title","others");
+        return "others";
+    }
+
+
+
+}
