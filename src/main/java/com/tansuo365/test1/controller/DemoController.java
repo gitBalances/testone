201cc841097d05a721bad8046fd05d193d767db3
deadzq <deadzq@qq.com>
2019-01-27 08:33:27 +0800

    2017-1-27

--- src/main/java/com/tansuo365/test1/controller/DemoController.java
+++ src/main/java/com/tansuo365/test1/controller/DemoController.java
@@ -9,31 +9,61 @@ public class DemoController {
 
     //form样式
     @RequestMapping("/formstyle")
-    public String formstyle(){
+    public String formstyle() {
         return "/webdemo/formstyle";
     }
 
-    //echarts样式0
+    //echarts样式
     @RequestMapping("/echarts")
-    public String echarts(){
+    public String echarts() {
         return "/webdemo/echarts";
+    }
+
+    //echarts样式0
+    @RequestMapping("/echarts0")
+    public String echarts0() {
+        return "/webdemo/echarts0";
+    }
+    //echarts样式01
+    @RequestMapping("/echarts01")
+    public String echarts01() {
+        return "/webdemo/echarts01";
     }
 
     //echarts样式1
     @RequestMapping("/echarts1")
-    public String echarts1(){
+    public String echarts1() {
         return "/webdemo/echarts1";
     }
+
     //echarts样式2
     @RequestMapping("/echarts2")
-    public String echarts2(){
+    public String echarts2() {
         return "/webdemo/echarts2";
     }
+
     //echarts样式3
     @RequestMapping("/echarts3")
-    public String echarts3(){
+    public String echarts3() {
         return "/webdemo/echarts3";
     }
 
+    //echartsdemo1
+    @RequestMapping("/echartsdemo1")
+    public String echartsdemo1() {
+        return "/webdemo/echartsdemo1";
+    }
+
+    //test1
+    @RequestMapping("/test1")
+    public String test1() {
+        return "/webdemo/test1";
+    }
+
+    //testLogin
+    @RequestMapping("/testLogin")
+    public String testLogin() {
+        return "/webdemo/testLogin";
+    }
 
 }
