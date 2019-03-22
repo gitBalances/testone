ba8e042afe3e2b738fa19cb002564c72bbfbda91
deadzq <deadzq@qq.com>
2019-03-22 08:26:46 +0800

    待上线测试前的提交 3-22

---
+++ src/main/java/com/tansuo365/test1/controller/db/DBBackUpQuartzJob.java
@@ -0,0 +1,11 @@
+package com.tansuo365.test1.controller.db;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class DBBackUpQuartzJob implements Job{
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        
+    }
+}
