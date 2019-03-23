0519423b948970c90b32c95c16443bdc0610aa1a
deadzq <deadzq@qq.com>
2019-03-23 15:16:28 +0800

    front前端页面banner待作为公共页

--- src/main/java/com/tansuo365/test1/controller/front/ShowPageController.java
+++ src/main/java/com/tansuo365/test1/controller/front/ShowPageController.java
@@ -16,7 +16,7 @@ public class ShowPageController {
     @ApiOperation(value="首页", notes="首页")
     @RequestMapping("/")
     public String index(Model model){
-//        model.addAttribute("title","有色网 测试网页!");
+        model.addAttribute("title","首页");
         return "/front/index";
     }
 
@@ -24,7 +24,7 @@ public class ShowPageController {
     @ApiOperation(value="资讯", notes="资讯")
     @RequestMapping("/information")
     public String information(Model model){
-//        model.addAttribute("title","关于碳索365");
+        model.addAttribute("title","资讯");
         return "/front/information";
     }
 
@@ -32,7 +32,7 @@ public class ShowPageController {
     @ApiOperation(value="产业地图", notes="产业地图")
     @RequestMapping("/map")
     public String map(Model model){
-//        model.addAttribute("title","关于碳索365");
+        model.addAttribute("title","产业地图");
         return "/front/map";
     }
 
@@ -40,7 +40,7 @@ public class ShowPageController {
     @ApiOperation(value="大宗原料", notes="大宗原料")
     @RequestMapping("/materials")
     public String materials(Model model){
-//        model.addAttribute("title","关于碳索365");
+        model.addAttribute("title","大宗原料");
         return "/front/materials";
     }
 
@@ -48,7 +48,7 @@ public class ShowPageController {
     @ApiOperation(value="行情", notes="行情")
     @RequestMapping("/quotation")
     public String quotation(Model model){
-//        model.addAttribute("title","关于碳索365");
+        model.addAttribute("title","行情");
         return "/front/quotation";
     }
 
@@ -68,17 +68,13 @@ public class ShowPageController {
         return "/front/s_year";
     }
 
-
-
-
-
     /*old set*/
 
     /*services*/
     @ApiOperation(value="服务", notes="服务")
     @RequestMapping("/services")
     public String services(Model model){
-        model.addAttribute("title","services");
+        model.addAttribute("title","服务");
         return "services";
     }
 
@@ -86,7 +82,7 @@ public class ShowPageController {
     @ApiOperation(value="联系", notes="联系")
     @RequestMapping("/contact")
     public String contact(Model model){
-        model.addAttribute("title","contact");
+        model.addAttribute("title","联系");
         return "contact";
     }
 
@@ -94,7 +90,7 @@ public class ShowPageController {
     @ApiOperation(value="其它", notes="其它")
     @RequestMapping("/others")
     public String others(Model model){
-        model.addAttribute("title","others");
+        model.addAttribute("title","其它");
         return "others";
     }
 
